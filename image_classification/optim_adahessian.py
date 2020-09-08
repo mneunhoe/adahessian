54e6add349e0388e7cc5939db483f391422eaefb
Marcel Neunhoeffer <mneunhoe@mail.uni-mannheim.de>
2020-09-08 19:27:42 +0200

    Update optim_adahessian.py

--- image_classification/optim_adahessian.py
+++ image_classification/optim_adahessian.py
@@ -25,6 +25,206 @@ from copy import deepcopy
 import numpy as np
 
 
+def try_update(params, step_size, params_current, search_direction):
+    zipped = zip(params, params_current, search_direction)
+
+    for p_next, p_current, search_direction in zipped:
+        #p_next.data = p_current - step_size * g_current
+        p_next.data = p_current + step_size * search_direction
+
+class Adahessian_sls(Optimizer):
+    """Implements Adahessian algorithm.
+    It has been proposed in `ADAHESSIAN: An Adaptive Second OrderOptimizer for Machine Learning`.
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate (default: 0.15)
+        betas (Tuple[float, float], optional): coefficients used for computing
+            running averages of gradient and its square (default: (0.9, 0.999))
+        eps (float, optional): term added to the denominator to improve
+            numerical stability (default: 1e-4)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        hessian_power (float, optional): Hessian power (default: 1)
+    """
+    def __init__(self, params, lr=0.15, betas=(0.9, 0.999), eps=1e-4,
+                 weight_decay=0, block_length=1, hessian_power=1, line_search=False):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError(
+                "Invalid beta parameter at index 0: {}".format(
+                    betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError(
+                "Invalid beta parameter at index 1: {}".format(
+                    betas[1]))
+        if not 0.0 <= hessian_power <= 1.0:
+            raise ValueError("Invalid Hessian power value: {}".format(hessian_power))
+        defaults = dict(lr=lr, betas=betas, eps=eps,
+                        weight_decay=weight_decay, hessian_power=hessian_power, line_search=line_search)
+        super(Adahessian, self).__init__(params, defaults)
+        self.block_length = block_length
+    def get_trace(self, gradsH):
+        """
+        compute the Hessian vector product with a random vector v, at the current gradient point,
+        i.e., compute the gradient of <gradsH,v>.
+        :param gradsH: a list of torch variables
+        :return: a list of torch tensors
+        """
+        params = self.param_groups[0]['params']
+        v = [torch.randint_like(p, high=2) for p in params]
+        for v_i in v:
+            v_i[v_i == 0] = -1
+        hvs = torch.autograd.grad(
+            gradsH,
+            params,
+            grad_outputs=v,
+            only_inputs=True,
+            retain_graph=True)
+            
+        hutchinson_trace = []
+        for hv, vi in zip(hvs, v):
+            param_size = hv.size()
+            if len(param_size) <= 1:  
+                # For 1D tensor, e.g.,, bias, BatchNorm, LayerNorm etc.
+                # Usually, you do not need to set spatial aveging for it, i.e., Hessian diagonal block size is 1 here.
+                tmp_output = torch.abs(hv * vi)
+                hutchinson_trace.append(tmp_output)
+                # Of course, you can also use the same way as 2D tensor does to average your 1D tensor. 
+                # tmp_output1 = torch.abs((hv * vi + 0.)).view(-1, self.block_length) # faltten to the N times self.block_length
+                # tmp_output2 = torch.abs(torch.sum(tmp_output1, dim=[1])).view(-1) / float(self.block_length)
+                # tmp_output3 = tmp_output2.repeat_interleave(self.block_length).view(param_size)
+                # hutchinson_trace.append(tmp_output3)
+            elif len(param_size) == 2: 
+                # For 2D tensor, e.g., the matrix in the fully-connected layer.
+                # This is a normal case for MLP, Transformer models. 
+                # Usually, a spatial averaging needs to be used here to get the best result.
+                # If you are not looking for the absolute best config, you may set it to be 1.
+                # In all of our experiments, we sill get pretty good performance.
+                tmp_output1 = torch.abs((hv * vi + 0.)).view(-1, self.block_length) # faltten to the N times self.block_length
+                tmp_output2 = torch.abs(torch.sum(tmp_output1, dim=[1])).view(-1) / float(self.block_length)
+                tmp_output3 = tmp_output2.repeat_interleave(self.block_length).view(param_size)
+                hutchinson_trace.append(tmp_output3)
+            elif len(param_size) == 3:
+                # For 3D tensor, e.g., the 1D Conv layer.
+                # This layer is usually used for Char-LM.
+                # First Way:
+                # Usually, you can set it to be the conv kernel size: in more details, for instance, your input/output channels are 20 and your kernel size is 5, 
+                # then the 1D Conv kernel is in size 20x20x5, you can average along the final dim, i.e., the block_length = 5
+                tmp_output = torch.abs(torch.sum(torch.abs(
+                    hv * vi), dim=[2], keepdim=True)) / vi[0, 1].numel() # torch.sum() reduces the dim 2， i.e. the size 5
+                # Second way:
+                # Of course, you can also use the same self.block_length to average the spatival Hessian of 3D kernel.
+                # tmp_output1 = torch.abs((hv * vi + 0.)).view(-1, self.block_length) # faltten to the N times self.block_length
+                # tmp_output2 = torch.abs(torch.sum(tmp_output1, dim=[1])).view(-1) / float(self.block_length)
+                # tmp_output3 = tmp_output2.repeat_interleave(self.block_length).view(param_size)
+                # hutchinson_trace.append(tmp_output3)
+            elif len(param_size) == 4:  
+                # For 4D tensor, e.g, the 2D Conv layer
+                # This layer is usually used for CV tasks.
+                # First Way:
+                # Usually, you can set it to be the conv kernel size: in more details, for instance, your input/output channels are 256 and your kernel size is 3x3, 
+                # then the 2D Conv kernel is in size 20x20x3x3, you can average along the last two dims, , i.e., the block_length = 9
+                tmp_output = torch.abs(torch.sum(torch.abs(
+                    hv * vi), dim=[2, 3], keepdim=True)) / vi[0, 1].numel() # torch.sum() reduces the dim 2/3.
+                hutchinson_trace.append(tmp_output)
+                # Second way:
+                # Of course, you can also use the same self.block_length to average the spatival Hessian of 4D kernel.
+                # tmp_output1 = torch.abs((hv * vi + 0.)).view(-1, self.block_length) # faltten to the N times self.block_length
+                # tmp_output2 = torch.abs(torch.sum(tmp_output1, dim=[1])).view(-1) / float(self.block_length)
+                # tmp_output3 = tmp_output2.repeat_interleave(self.block_length).view(param_size)
+                # hutchinson_trace.append(tmp_output3)
+        return hutchinson_trace
+    def step(self, gradsH, closure=None):
+        """Performs a single optimization step.
+        Arguments:
+            gradsH: The gradient used to compute Hessian vector product.
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        seed = time.time()
+        def closure_deterministic():
+            with random_seed_torch(int(seed)):
+                return closure()
+        params = self.param_groups[0]['params']
+        params_current = copy.deepcopy(params)
+        loss = None
+        if closure is not None:
+            loss = closure_deterministic()
+        # get the Hessian diagonal
+        hut_trace = self.get_trace(gradsH)
+        for group in self.param_groups:
+            for i, p in enumerate(group['params']):
+                if p.grad is None:
+                    continue
+                p_current = deepcopy(p.data)
+                grad = deepcopy(gradsH[i].data)
+                state = self.state[p]
+                # State initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    # Exponential moving average of Hessian diagonal square values
+                    state['exp_hessian_diag_sq'] = torch.zeros_like(p.data)
+                exp_avg, exp_hessian_diag_sq = state['exp_avg'], state['exp_hessian_diag_sq']
+                beta1, beta2 = group['betas']
+                lr = group['lr']
+                state['step'] += 1
+                # Decay the first and second moment running average coefficient
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                exp_hessian_diag_sq.mul_(beta2).addcmul_(
+                    1 - beta2, hut_trace[i], hut_trace[i])
+                bias_correction1 = 1 - beta1 ** state['step']
+                bias_correction2 = 1 - beta2 ** state['step']
+                # make the square root, and the Hessian power
+                k = group['hessian_power']
+                denom = (
+                    (exp_hessian_diag_sq.sqrt() ** k) /
+                    math.sqrt(bias_correction2) ** k).add_(
+                    group['eps'])
+                
+                if group['line_search']:
+                    
+                    search_direction = -1* (exp_avg / bias_correction1 / denom + group['weight_decay'] * p.data)
+                    
+                    line_m = torch.matmul(grad, search_direction.t())
+                    
+                    prev_loss = loss
+                    c = 0.5
+                    tau = 0.5
+                    search_t = -c*line_m
+                    alpha = lr
+                    try_update(params, alpha, params_current, search_direction)
+                    loss_next = closure_deterministic()
+                    ls_it = 1
+                    while (prev_loss - loss_next) < (alpha * search_t) and ls_it < 100 :
+                            alpha = tau * alpha
+                            try_update(params, alpha, params_current, search_direction)
+                            loss_next = closure_deterministic()
+                            ls_it = ls_it + 1
+                    lr = alpha
+                    # make update
+                p.data = p.data - \
+                   lr * (exp_avg / bias_correction1 / denom + group['weight_decay'] * p.data)
+        return loss
+
+def get_params_grad(model):
+    """
+    get model parameters and corresponding gradients
+    """
+    params = []
+    grads = []
+    for n, param in model.named_parameters():
+        #print(n, param.shape, param.requires_grad, None if param.grad is None else param.grad.shape)
+        if not param.requires_grad:# or "decoder.sentence_encoder.embed" in n:
+            continue
+        params.append(param)
+        grads.append(0. if param.grad is None else param.grad + 0.)
+    return params, grads
+
 class Adahessian(Optimizer):
     """Implements Adahessian algorithm.
     It has been proposed in `ADAHESSIAN: An Adaptive Second OrderOptimizer for Machine Learning`.
