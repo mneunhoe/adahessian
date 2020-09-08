5a3a380c7472c6313fe0c8b5b97c93b4627e2aa6
Marcel Neunhoeffer <mneunhoe@mail.uni-mannheim.de>
2020-09-08 20:05:02 +0200

    Update optim_adahessian.py

--- image_classification/optim_adahessian.py
+++ image_classification/optim_adahessian.py
@@ -25,6 +25,31 @@ from copy import deepcopy
 import numpy as np
 import time
 
+@contextlib.contextmanager
+def random_seed(seed):
+    state = np.random.get_state()
+    np.random.seed(seed)
+    try:
+        yield
+    finally:
+        np.random.set_state(state)
+
+@contextlib.contextmanager
+def random_seed_torch(seed, device=0):
+    cpu_rng_state = torch.get_rng_state()
+    #gpu_rng_state = torch.cuda.get_rng_state(0)
+
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    try:
+        yield
+    finally:
+        torch.set_rng_state(cpu_rng_state)
+        torch.cuda.set_rng_state(gpu_rng_state, device)
+
+
 class Adahessian_sls(Optimizer):
     """Implements Adahessian algorithm.
     It has been proposed in `ADAHESSIAN: An Adaptive Second OrderOptimizer for Machine Learning`.
