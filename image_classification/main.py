d51c5c5871f24e960efe33b7690b524cdae025ed
Marcel Neunhoeffer <mneunhoe@mail.uni-mannheim.de>
2020-09-08 19:46:21 +0200

    Update main.py

--- image_classification/main.py
+++ image_classification/main.py
@@ -62,7 +62,7 @@ parser.add_argument('--depth', type=int, default=20,
                     help='choose the depth of resnet')
 parser.add_argument('--optimizer', type=str, default='adahessian',
                     help='choose optim')
-parser.add_argument('--schedule', type=bool, default=True',
+parser.add_argument('--schedule', type=bool, default=True,
                     help='turn on lr scheduler')
 
 args = parser.parse_args()
