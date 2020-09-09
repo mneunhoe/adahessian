ee777e8525f06dfcf5e7ba118f49511c3452a77e
Marcel Neunhoeffer <mneunhoe@mail.uni-mannheim.de>
2020-09-09 10:30:52 +0200

    Update main.py

--- image_classification/main.py
+++ image_classification/main.py
@@ -120,7 +120,7 @@ elif args.optimizer == 'adahessian_sls':
     args.weight_decay = 0
     optimizer = Adahessian_sls(
         model.parameters(),
-        lr=10,
+        lr=1,
         weight_decay=args.weight_decay)
 else:
     raise Exception('We do not support this optimizer yet!!')
